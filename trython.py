dd86a2382133aa8df9789970948bafa1cd0617ca
nicolas <mkleinnicolas@gmail.com>
2023-01-03 15:18:06 +0100

    first ligne tkinter

--- trython.py
+++ trython.py
@@ -3,6 +3,12 @@ from tkinter import *
 fenetre = Tk()
 
 label = Label(fenetre, text="Hello World")
-label.pack()
+
+p = PanedWindow(fenetre, orient=HORIZONTAL)
+p.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
+p.add(Label(p, text='Volet 1', background='blue', anchor=CENTER))
+p.add(Label(p, text='Volet 2', background='white', anchor=CENTER) )
+p.add(Label(p, text='Volet 3', background='red', anchor=CENTER) )
+p.pack()
 
 fenetre.mainloop()
