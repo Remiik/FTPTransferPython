11b72ad98759c5ceacc31e69d4a960bf70760b25
Rmk <rkrebs2001@free.fr>
2023-01-03 15:52:46 +0100

    ajouDeMerde

--- trython.py
+++ trython.py
@@ -1,14 +1,56 @@
-from tkinter import * 
+# pip install kivy
 
-fenetre = Tk()
+from tkinter.tix import ButtonBox
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
-label = Label(fenetre, text="Hello World")
+class SayHello(App):
+	def build(self):
+		self.window = GridLayout()
+		self.window.cols=1
+		self.window.size_hint = (0.5, 0.5)
+		self.window.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+		self.image = Image(source="2133.JPG")
+		# on créer l'objet Label
+		self.window.add_widget(self.image)
+  
+		self.message = Label(text="Bonjour de Malte")
+		# on ajoute le label à la fenêtre
+		self.window.add_widget(self.message)
+		self.user = TextInput(text="", size_hint = (1,0.2), multiline = False)
+		self.window.add_widget(self.user)
+		# création de la zone des boutons
+		self.buttonArea = GridLayout(cols=2, size_hint = (1,0.2))
+		# création du premier bouton	
+		self.button = Button(text="Say Hello", 
+				size_hint = (1,0.2),
+				on_press=self.buttonHello
+		)
+		# creation du second bouton
+		self.buttonQuit = Button(text="Quit", 
+				size_hint = (1,0.2),
+				on_press=self.buttonQuit
+		)
+		# ajout des boutons à la zone des boutons
+		self.buttonArea.add_widget(self.button)
+		self.buttonArea.add_widget(self.buttonQuit)
+		# ajout de la zone des boutons à la fenêtre
+		self.window.add_widget(self.buttonArea)
+		# affiche la fenetre construite	
+		return self.window
 
-p = PanedWindow(fenetre, orient=HORIZONTAL)
-p.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
-p.add(Label(p, text='Volet 1', background='blue', anchor=CENTER))
-p.add(Label(p, text='Volet 2', background='white', anchor=CENTER) )
-p.add(Label(p, text='Volet 3', background='red', anchor=CENTER) )
-p.pack()
+	def buttonHello(self, instance):
+		self.message.text = "Bonjour " + self.user.text
 
-fenetre.mainloop()
+	def buttonQuit(self, instance):
+		self.stop()
+
+if __name__ == "__main__":
+	SayHello().run()
+
+if ButtonBox == 3:
+    print("coucou")
